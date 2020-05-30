eb5951e0ce5453f2f90727974ede4f778f373c1d
Ondra <ondra@LAPTOP-L3CSP7V1>
2020-05-30 18:54:59 +0200

    No konecne hotove to pridavani jidla :D eeeeeej

--- jidlo.php
+++ jidlo.php
@@ -1,6 +1,5 @@
 <?php
 require_once "inc/header.php";
-
 #nacteni zvoleneho jidelnicku
 $queryzvolen= $db->prepare('Select jidelnicek_id from uzivatele where id=:iduser');
 $queryzvolen->execute([':iduser'=>$_SESSION['user_id']]);
@@ -8,11 +7,37 @@ $zvoleny= $queryzvolen->fetch(PDO::FETCH_ASSOC);
 $queryjidelnicek = $db->prepare('Select * from jidelnicek where id=:idjidelnicek;');
 $queryjidelnicek->execute([':idjidelnicek'=>$zvoleny['jidelnicek_id']]);
 $jidelnicek= $queryjidelnicek->fetch(PDO::FETCH_ASSOC);
+
+
+#zvoleni dne a ulezeni do promene
+if(!empty($_POST)){
+   $datum=$_POST['datum'];#v metode post jde vzdy info o datumu
+
+    #ulozeni snedeneho jidla
+    if(!empty($_POST['idjidlo'])&& $_POST['stav']=='vybrat'){
+        $querypridat= $db->prepare('INSERT INTO den (uzivatele_id, jidlo_id, datum, jidelnicek) VALUES (:iduzivatele, :idjidla, :datum, :jidelnicek );');
+        $querypridat->execute([':iduzivatele'=>$_SESSION['user_id'],
+                                ':idjidla'=>$_POST['idjidlo'],
+                                ':datum'=>$_POST['datum'],
+                                ':jidelnicek'=>$zvoleny['jidelnicek_id']]);
+    }
+    if(!empty($_POST['idjidlo'])&& $_POST['stav']=='snedeno'){#smazani snedeneho jidla
+        $querysmazat= $db->prepare('DELETE FROM den WHERE datum=:datum AND jidlo_id=:idjidlo ');
+        $querysmazat->execute([':datum'=>$_POST['datum'],
+                                ':idjidlo'=>$_POST['idjidlo']]);
+
+    }
+}else{#pri nacteni straanky
+    $datum=date('Y-m-d');
+}
+#ulozeni snedeneho jidla
+
+
 #nacteni dnesniho jidla a snedenych tech blbu
 $querydnesek= $db->prepare('SELECT jidlo_id FROM den WHERE uzivatele_id=:iduzivatele and datum=:dneska;');
 $querydnesek->execute([
     ':iduzivatele'=>$_SESSION['user_id'],
-    ':dneska'=>date('Y-m-d')
+    ':dneska'=>$datum
 ]);
 #nacteni dneska uz snedenych jidel
 $idjidla = $querydnesek->fetchAll(PDO::FETCH_ASSOC);
@@ -34,8 +59,15 @@ foreach ($dnesnijidla as $dnesnijidlo){
 }
 ?>
     <div class="">
+        <div class="form-group">
+            <form method="post">
+                <label for="datum">Datum:</label>
+                <input type="date" name="datum" id="datum" value="<?php echo $datum;?>" />
+                <button type="submit" class="btn btn-outline-primary" >Zvolit</button>
+            </form>
+        </div>
 <h3>
-Dnešní statistika:
+Statistika:
     <small class="text-muted">
         <?php
         echo "cukry ".$cukry."/".$jidelnicek['cukry']."g, sacharidy ".$sacharidy."/".$jidelnicek['sacharidy']."g, bílkoviny ".$bilkoviny."/".$jidelnicek['bilkoviny']."g ";
@@ -73,7 +105,10 @@ Dnešní statistika:
             <tr>
                 <th> <?php echo $jidlo['nazev']; ?></th>
                 <th>
-                    <button type="button" class="btn btn-outline-info" href="popisjidla.php">Info</button>
+                    <form method="get" action="popisjidla.php">
+                        <input type="hidden" name="idjidlo" value="<?php echo $jidlo['id']; ?>">
+                        <button type="submit" class="btn btn-outline-info">Info</button>
+                    </form>
                 </th>
                 <th>
 
@@ -88,7 +123,10 @@ Dnešní statistika:
                             ?>
                             <form method="post">
                                 <input type="hidden" name="idjidlo" value="<?php echo $jidlo['id']; ?>"/>
+                                <input type="hidden" name="datum" value="<?php echo $datum; ?>"/>
+                                <input type="hidden" name="stav" value="snedeno"/>
                                 <button type="submit" class="btn btn-outline-warning">Snědeno</button>
+
                             </form>
                             <?
                         }
@@ -97,6 +135,8 @@ Dnešní statistika:
                             ?>
                             <form method="post">
                                 <input type="hidden" name="idjidlo" value="<?php echo $jidlo['id']; ?>"/>
+                                <input type="hidden" name="datum" value="<?php echo $datum; ?>"/>
+                                <input type="hidden" name="stav" value="vybrat"/>
                                 <button type="submit" class="btn btn-outline-success">Vybrat</button>
                             </form>
                             <?php
@@ -111,9 +151,81 @@ Dnešní statistika:
         }
         ?>
     </table>
-
 </div>
+<div>
+    <table class="table">
+        <thead class="thead-light">
+        <tr>
+            <th colspan="3" class="text-center">Vlastní Jídla</th>
+        </tr>
+        <tr>
+            <th> Název</th>
+            <th> Popis</th>
+            <th> Stav</th>
+        </tr>
+        </thead>
+        <?php #vybrani vlastnich jidel
+            $queryvlastni = $db->prepare('SELECT * FROM jidlo WHERE vlastnik=:iduzivatele');
+            $queryvlastni->execute([':iduzivatele'=>$_SESSION['user_id']]);
+            $vlastni = $queryvlastni->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($vlastni as $jidlo){
+            ?>
+            <tr>
+                <th> <?php echo $jidlo['nazev']; ?></th>
+                <th>
+                    <form method="get" action="popisjidla.php">
+                        <input type="hidden" name="idjidlo" value="<?php echo $jidlo['id']; ?>">
+                        <button type="submit" class="btn btn-outline-info">Info</button>
+                    </form>
+                </th>
+                <th>
 
+                    <?php
+                    $pomoc=0;#pomocna promenna
+                    foreach ($dnesnijidla as $porovnani){
+                        if($porovnani['id']==$jidlo['id']){
+                            $pomoc=1;
+                        }
+                    }
+                    if($pomoc==1){
+                        ?>
+                        <form method="post">
+                            <input type="hidden" name="idjidlo" value="<?php echo $jidlo['id']; ?>"/>
+                            <input type="hidden" name="datum" value="<?php echo $datum; ?>"/>
+                            <input type="hidden" name="stav" value="snedeno"/>
+                            <button type="submit" class="btn btn-outline-warning">Snědeno</button>
+
+                        </form>
+                        <?
+                    }
+                    else
+                    {
+                        ?>
+                        <form method="post">
+                            <input type="hidden" name="idjidlo" value="<?php echo $jidlo['id']; ?>"/>
+                            <input type="hidden" name="datum" value="<?php echo $datum; ?>"/>
+                            <input type="hidden" name="stav" value="vybrat"/>
+                            <button type="submit" class="btn btn-outline-success">Vybrat</button>
+                        </form>
+                        <?php
+
+                    }
+                    ?>
+
+                </th>
+            </tr>
+
+            <?php
+        }
+
+        ?>
+        <tr>
+            <th>
+                <a type="button" class="btn btn-outline-primary" href="pridatvlastni.php">Přidat vlastní</a>
+            </th>
+        </tr>
+    </table>
+</div>
 
 
 
